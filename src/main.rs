04df7fe39c29c3c58232f33f4211a06f442d71f6
Johannes Huwald <johannes.huwald@gmail.com>
2021-06-26 09:31:04 +0200

    Create minimal sound example with cpal
    Initialize a stream and generate a constant sine wave of 440.0 Hz.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+
+fn output_sound_value(time: f32) -> f32 {
+    (time * 440.0 * 2.0 * std::f32::consts::PI).sin()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let host = cpal::default_host();
+    let device = host.default_output_device().expect("no output device available");
+
+    let mut supported_configs_range = device.supported_output_configs()
+        .expect("error while querying configs");
+
+    let config = supported_configs_range.next()
+        .expect("no supported config?!")
+        .with_max_sample_rate();
+
+    match config.sample_format() {
+        cpal::SampleFormat::F32 => run::<f32>(&device, &config.into()),
+        cpal::SampleFormat::I16 => run::<i16>(&device, &config.into()),
+        cpal::SampleFormat::U16 => run::<u16>(&device, &config.into()),
+    };
+}
+
+pub fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig)
+where
+    T: cpal::Sample,
+{
+    let sample_rate = config.sample_rate.0 as f32;
+    let channels = config.channels as usize;
+
+    // Produce a sinusoid of maximum amplitude.
+    let mut sample_clock = 0f32;
+    let mut next_value = move || {
+        sample_clock = (sample_clock + 1.0) % sample_rate;
+        output_sound_value(sample_clock / sample_rate)
+    };
+
+    let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
+
+    let stream = device.build_output_stream(
+        config,
+        move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
+            write_data(data, channels, &mut next_value)
+        },
+        err_fn,
+    ).expect("Unable to build output stream");
+
+    stream.play().expect("Unable to play stream");
+
+    loop { }
+}
+
+fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> f32)
+where
+    T: cpal::Sample,
+{
+    for frame in output.chunks_mut(channels) {
+        let value: T = cpal::Sample::from::<f32>(&next_sample());
+        for sample in frame.iter_mut() {
+            *sample = value;
+        }
+    }
 }
